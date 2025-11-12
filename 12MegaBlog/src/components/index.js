592681bbeaba403e8cb1b06ea2a962da75be9dd3
Umair Ali <umair.fullstackdev@gmail.com>
2025-11-12 05:19:57 +0500

    feat: enhance Header component with navigation and logo integration

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -1,7 +1,13 @@
 import Header from "./header/header";
 import Footer from "./footer/footer";
+import container from './container/container'
+import logo from './logo'
+import logoutBtn from './header/logoutBtn'
 
 export {
     Header,
     Footer,
+    container,
+    logo,
+    logoutBtn
 }
