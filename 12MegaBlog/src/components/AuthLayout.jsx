f992435bf014f0fbd59cc27f5896f1049e8eaacd
Umair Ali <umair.fullstackdev@gmail.com>
2025-11-15 05:10:17 +0500

    style: format code for consistency in AuthLayout component

--- 12MegaBlog/src/components/AuthLayout.jsx
+++ 12MegaBlog/src/components/AuthLayout.jsx
@@ -1,14 +1,14 @@
-import React, {useState, useEffect} from 'react'
-import {useSelector} from 'react-redux'
-import {useNavigate} from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
-export default function Protected({children, authentication = true}) {
+export default function Protected({ children, authentication = true }) {
     const navigate = useNavigate()
     const [loader, setLoader] = useState(true)
     const authStatus = useSelector(state => state.auth.status)
 
     useEffect(() => {
-        if (authentication && authStatus !== authentication){
+        if (authentication && authStatus !== authentication) {
             navigate("/login")
         } else if (!authentication && authStatus !== authentication) {
             navigate("/")
